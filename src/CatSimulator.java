6ba87ceaa4eb33b46b0bbed7e1e1f3660f32f809
Justin-K-Ellis <jlke45@gmail.com>
2025-05-20 11:09:41 +0900

    feat: create main game loop

--- src/CatSimulator.java
+++ src/CatSimulator.java
@@ -1,9 +1,47 @@
+import java.util.Scanner;
+
 public class CatSimulator {
     public static void main(String[] args) {
-        System.out.println("Welcome to Cat Simulator!");
+        Scanner scanner = new Scanner(System.in);
 
-        Cat mou = new Cat("Mouchan", 4, "black and white", "male", true);
-        System.out.println(mou.describe());
-//        mou.neuter();
+        System.out.println("Welcome to Cat Simulator!");
+        System.out.println("First, tell me about your cat.");
+        System.out.println("What is their name?");
+        String name = scanner.nextLine();
+        System.out.println("How old are they?");
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println("What color are they?");
+        String color = scanner.nextLine();
+        System.out.println("Male or female?");
+        String sex = scanner.nextLine();
+        Cat cat = new Cat(name, age, color, sex);
+        System.out.println(cat.describe());
+
+
+        String mainPrompt = "What do you want to do?";
+        mainPrompt += "\n1. Pet cat.";
+        mainPrompt += "\n2. Feed cat.";
+        mainPrompt += "\n3. Neuter cat.";
+        mainPrompt += "\n4. Describe cat.";
+        mainPrompt += "\n5. Quit.";
+
+
+        while (true) {
+            System.out.println(mainPrompt);
+            String answer = scanner.nextLine();
+            switch (answer) {
+                case "1":
+                    cat.pet();
+                case "2":
+                    //todo
+                case "3":
+                    cat.neuter();
+                case "4":
+                    cat.describe();
+                case "5":
+                    System.out.println("Thank you for playing Cat Simulator.");
+                    break;
+            }
+        }
     }
 }
