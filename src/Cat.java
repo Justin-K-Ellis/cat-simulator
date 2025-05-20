90db274615519642d97f744f6057a8b9c99b2dbc
Justin-K-Ellis <jlke45@gmail.com>
2025-05-20 10:10:52 +0900

    feat: create describe and neuter methods

--- src/Cat.java
+++ src/Cat.java
@@ -1,3 +1,36 @@
-public class Cat {
+import java.text.MessageFormat;
 
+public class Cat {
+    private String name;
+    private int age;
+    private String color;
+    private String sex;
+    private boolean isNeutered;
+
+    public Cat(String name, int age, String color, String sex, boolean isNeutered) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.sex = sex;
+        this.isNeutered = isNeutered;
+    }
+
+    public String describe() {
+        String pronoun = this.sex.equals("male") ? "He" : "She";
+        String neuteredStatus = this.isNeutered ? "is" : "is not";
+        String description = "";
+        description += MessageFormat.format("{0} is {1}.", this.name, this.color);
+        description += MessageFormat.format(" {0} {1} neutered.", pronoun, neuteredStatus);
+        return description;
+    }
+
+    public void neuter() {
+        if (!this.isNeutered) {
+            this.isNeutered = true;
+            System.out.println(MessageFormat.format("{0}: 'Meeooow!'", this.name));
+            System.out.println(MessageFormat.format("{0} has been neutered!", this.name));
+        } else {
+            System.out.println(MessageFormat.format("{0} has already been neutered!", this.name));
+        }
+    }
 }
--- src/CatSimulator.java
+++ src/CatSimulator.java
@@ -1,5 +1,9 @@
 public class CatSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Cat Simulator!");
+
+        Cat mou = new Cat("Mouchan", 4, "black and white", "male", true);
+        System.out.println(mou.describe());
+        mou.neuter();
     }
 }
